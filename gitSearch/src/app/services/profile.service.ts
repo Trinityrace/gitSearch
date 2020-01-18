0cf22de0864a307018f19517b81f226a4b1a4b25
Trinity John <trinityrtime@gmail.com>
2020-01-19 01:41:50 +0300

    services input and binding data

--- gitSearch/src/app/services/profile.service.ts
+++ gitSearch/src/app/services/profile.service.ts
@@ -11,5 +11,14 @@ export class ProfileService {
   private username:string;
   private clientid='3dc51f7ff967cb0e6e0e';
   private clientsecret = '7fa79c1f2aea279a11a487161f3632194fe3c903';
-  constructor(private http:Http) { }
+
+
+  constructor(private http:Http) { 
+    console.log()"service is ready!";
+    this.username= "Trinityrace";
+  }
+  getProfileInfo(){
+    return this.http.get("https://api.github.com/users/"= this.username +"?client_id="= this.clientid +"&client_secret="+this.clientsecret)
+    .map(res.json());
+  }
 }
